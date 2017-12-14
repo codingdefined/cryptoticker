c24b7152c18aef8b4a3ae072e4bf5794007312f9
Hemant Joshi <codingdefined@gmail.com>
2017-12-14 18:40:45 +0530

    Added World Coin Index Price Feed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const https = require('https');
 const request = require('request');
 const cryptoValues = require("./crypto.json");
 const coinMarketCapUrl = "http://api.coinmarketcap.com/v1/ticker/";
+const worldCoinIndexURL = "https://www.worldcoinindex.com/apiservice/json?key=";
 var result = [];
 
 exports.price = (crypto) => {
@@ -17,6 +18,30 @@ exports.price = (crypto) => {
                 if(!error && res.statusCode === 200) {
                     const response = JSON.parse(body);
                     resolve(response);
+                }else {
+                    reject(new Error('Coin Not Found'));
+                }
+            });
+        }
+    });
+}
+
+exports.priceWCI = (crypto, key) => {
+    return new Promise((resolve, reject) => {
+        if(!crypto){
+            reject(new Error('Coin Not Found'));
+        }else if(!key){
+            reject(new Error('Key Not Provided'));
+        }else{
+            const cryptoId = cryptoValues[crypto.toLowerCase()] || crypto.toLowerCase();
+            const requestUrl = worldCoinIndexURL + key;
+            request(requestUrl, function (error, res, body) {
+                if(!error && res.statusCode === 200) {
+                    const response = JSON.parse(body);
+                    var filteredJson = response.Markets.filter(function(cryptoValue) {
+                        return cryptoValue.Name.toLowerCase() === cryptoId;
+                    });
+                    resolve(filteredJson);
                 }else {
                     reject(new Error('Coin Not Found'));
                 }
