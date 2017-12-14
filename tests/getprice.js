0a8cf44fbd57e2ad76ca6b4186602bd4e44c0503
Hemant Joshi <codingdefined@gmail.com>
2017-12-14 18:38:18 +0530

    Added Tests for World Coin Index

--- tests/getprice.js
+++ tests/getprice.js
@@ -4,3 +4,6 @@ crypto.price('btc').then(a=>console.log(a));
 crypto.price('eth').then(a=>console.log(a));
 crypto.price().then(a=>console.log(a));
 crypto.price('ahol').then(a=>console.log(a));
+
+crypto.priceWCI('eth','WorldCoinIndex Key').then(a=>console.log(a));
+crypto.priceWCI('bitcoin','WorldCoinIndex Key').then(a=>console.log(a));
